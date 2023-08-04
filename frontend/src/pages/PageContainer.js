1cfc0cad18f2e35be0138d1e964e5ae27e479f81
Dalaxshan <ujanth98@gmail.com>
2023-08-04 15:17:11 +0530

    Implement DOMPurify package into function

--- frontend/src/pages/PageContainer.js
+++ frontend/src/pages/PageContainer.js
@@ -1,26 +1,32 @@
 import React, { useEffect } from "react";
 import { EditorState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
-import {convertToHTML} from 'draft-convert';
+import { convertToHTML } from "draft-convert";
+import DOMPurify from "dompurify";
 
 import "../App.css";
 import "../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import { useState } from "react";
 
 export default function PageContainer() {
+  //editor State get updated values from the setEditorState when modify
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
 
-  const [convertedContent,setConvertContent]=useState(null);
+  const [convertedContent, setConvertContent] = useState(null);
 
-useEffect(()=>{
-  let html=convertToHTML(editorState.getCurrentContent());
-setConvertContent(html);
+  useEffect(() => {
+    let html = convertToHTML(editorState.getCurrentContent());//get updated value from editorState and convert into HTML
+    setConvertContent(html);
+  }, [editorState]);
 
-},[editorState]);
-
-console.log(convertedContent);
+  //Get the HTML value then return the sanatized value
+  function createMarkup(html) {
+    return {
+      __html: DOMPurify.sanitize(html)
+    };
+  }
 
   return (
     <div>
@@ -34,6 +40,12 @@ console.log(convertedContent);
         editorClassName="editor-class"
         toolbarClassName="toolbar-class"
       />
+
+<div
+    className="preview"
+    dangerouslySetInnerHTML={createMarkup(convertedContent)}>
+  </div>
+
     </div>
   );
 }
